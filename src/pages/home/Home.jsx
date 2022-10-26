edf9d67de52163a27968de095959069b9a2899b6
salutJuillet <salut.juilllet@gmail.com>
2022-10-26 15:38:42 +0900

    Update Home.jsx
    Home을 List와 똑같이 복사

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import './home.css'
 import Header from '../../comp/header/Header';
+import Posts from '../../comp/posts/Posts';
+import Sidebar from '../../comp/sidebar/Sidebar';
 
 export default function Home() {
   return (
     <>
         <Header />
-        <div className="container home">
+        <div className="container list">
+            <div className="post">
+              <Posts />
+              <Posts />
+              <Posts />
+              <Posts />
+              <Posts />
+              <Posts />
+            </div>
+            <Sidebar />
         </div>
     </>
   )
